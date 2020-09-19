be1a7c6733e54371edbe926c2890f82419b75691
Luís Henriques <luis.henriques@uniplaces.com>
2020-09-19 11:20:54 +0100

    Add dynamodb client

---
+++ src/infrastructure/dynamodb.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const promise = require('bluebird');
+
+AWS.config.setPromisesDependency(promise);
+
+let options = {};
+// we enter here if we are running a local dynamodb
+if (process.env.IS_OFFLINE) {
+  const host = process.env.DYNAMODB_HOST || 'localhost';
+  options = {
+    region: 'localhost',
+    endpoint: `http://${host}:8000`,
+    accessKeyId: 'MOCK_ACCESS_KEY_ID',
+    secretAccessKey: 'MOCK_SECRET_ACCESS_KEY',
+  };
+}
+
+// we enter here if we are running tests
+if (process.env.JEST_WORKER_ID) {
+  options = {
+    endpoint: process.env.MOCK_DYNAMODB_ENDPOINT,
+    region: 'local',
+    sslEnabled: false
+  };
+}
+
+module.exports = new AWS.DynamoDB.DocumentClient(options);
